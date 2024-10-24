ced8a050604023868ae3410d946fc642783fbc31
dev-NAE <gkskfh1324@gmail.com>
2024-10-24 17:56:30 +0900

    [24.10.24 강내호] - 재고관리 페이지, 컨트롤러 생성

---
+++ src/main/java/com/itwillbs/controller/InventoryController.java
@@ -0,0 +1,16 @@
+package com.itwillbs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class InventoryController {
+
+	@GetMapping("/inventoryList")
+	public String inventoryList() {
+		
+		
+		return "inventory/inventoryList";
+	}
+	
+}
