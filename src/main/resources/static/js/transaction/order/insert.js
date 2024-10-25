d6a27032e188759af47cbb5981e387551436d16a
g-yada <leeej0217@gmail.com>
2024-10-25 13:59:37 +0900

    발주등록페이지 기능구현 - 등록 외 완료

--- src/main/resources/static/js/transaction/order/insert.js
+++ src/main/resources/static/js/transaction/order/insert.js
@@ -19,7 +19,10 @@ $('#find-manager').on('click', function(event) {
 });
 
 // 팝업에서 선택한 담당자 폼에 입력
-
+function setManager(managerCode, managerName) {
+    $('#manager-code').val(managerCode);
+    $('#manager-name').val(managerName);
+}
 
 // 거래처 검색 팝업
 $('#find-supplier').on('click', function(event) {
@@ -27,37 +30,64 @@ $('#find-supplier').on('click', function(event) {
 });
 
 // 팝업에서 선택한 거래처 폼에 입력
-
-
+function setSupplier(supplierCode, supplierName) {
+    $('#supplier-code').val(supplierCode);
+    $('#supplier-name').val(supplierName);
+}
 
 // 물품 추가 팝업
 $('#add-items').on('click', function(event) {
     openPopupAtMousePosition(event, '/addItems', 800, 600)
 });
 
+// 팝업에서 선택한 물품 목록에 입력
+function setItemInfo(itemCode, itemName) {
+    let $newItemRow = $($('#add-item-template').html());
+    $newItemRow.find('.item-code').text(itemCode);
+    $newItemRow.find('.item-name').text(itemName);
 
-// 팝업에서 선택한 물품 목록(+히든 인풋)에 입력
+    $('#item-list-table').append($newItemRow);
+    refreshQuantity();
+}
 
+// 단가, 수량 입력 시 소계, 총계 자동 산출
+$(document).on('input', '.item-price, .item-quantity', function() {
+    const $row = $(this).closest('.item-row');
+    const price = parseInt($row.find('.item-price').val()) || 0;
+    const quantity = parseInt($row.find('.item-quantity').val()) || 0;
+    const subtotal = price * quantity;
 
+    // 계산용 데이터 저장
+    $row.find('.subtotal').data('subtotal', subtotal);
 
-// 단가 * 수량 = 소계 자동 산출
-$(document).ready(function() {
-    $('.item-price, .item-quantity').on('input', function() {
-        const $row = $(this).closest('.item-row');
-        const price = parseInt($row.find('.item-price').val()) || 0;
-        const quantity = parseInt($row.find('.item-quantity').val()) || 0;
-
-        const subtotal = price * quantity;
-        $row.find('.subtotal').text(subtotal);
-    });
+    // 데이터 표시
+    $row.find('.subtotal').text(subtotal.toLocaleString());
+    refreshTotalPrice();
 });
 
 // 등록된 물품 지우기
-$('#delete-this-row').on('click', function(event) {
-
+$(document).on('click', '.delete-this-row', function() {
+    const $row = $(this).closest('.item-row');
+    $row.remove();
+    refreshQuantity();
+    refreshTotalPrice();
 });
 
 // 등록에 따른 총 품목개수, 합계금액 보여주기
+function refreshQuantity() {
+    var rowCount = $('#item-list-table .item-row').length;
+    $('#total-quantity').text(rowCount);
+}
+
+function refreshTotalPrice() {
+    let totalPrice = 0;
+    $('.subtotal').each(function() {
+        let subtotalVal = $(this).data('subtotal') || 0;
+        totalPrice += subtotalVal;
+    })
+    $('#total-price').data('totalPrice', totalPrice);
+    $('#total-price').text(totalPrice.toLocaleString());
+}
 
 
 // 발주등록 버튼: 필수항목이 비어있으면 비활성화, 모두 채워지면 활성화
