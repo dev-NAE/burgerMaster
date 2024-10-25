77dd3d0ce2a9ba5eb80bdb97f077c177bdb6f4f6
soyeon-123 <soddong134@naver.com>
2024-10-25 13:56:44 +0900

    글씨체 변경

---
+++ src/main/java/com/itwillbs/controller/MainAjaxController.java
@@ -0,0 +1,22 @@
+package com.itwillbs.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainAjaxController {
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}//
