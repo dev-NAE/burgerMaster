616c4b930fe49cc0aae1510b231c857b8b6da70d
dutper <dutper1997@gmail.com>
2024-10-28 17:44:49 +0900

    엔티티 생성

---
+++ src/main/java/com/itwillbs/entity/ManuOrder.java
@@ -0,0 +1,18 @@
+package com.itwillbs.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "manufacture_order")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class ManuOrder {
+	
+}
