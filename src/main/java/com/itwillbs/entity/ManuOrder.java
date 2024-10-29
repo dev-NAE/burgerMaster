663d3fe289ccb636c6409e8b183cf8fffbb4ba1f
dutper <dutper1997@gmail.com>
2024-10-29 17:38:29 +0900

    temp

--- src/main/java/com/itwillbs/entity/ManuOrder.java
+++ src/main/java/com/itwillbs/entity/ManuOrder.java
@@ -1,6 +1,10 @@
 package com.itwillbs.entity;
 
+import java.sql.Timestamp;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,4 +19,22 @@ import lombok.ToString;
 @NoArgsConstructor
 public class ManuOrder {
 	
+	@Id
+	@Column(name = "mOrder_id", length = 20, nullable = false)
+	private String mOrderId;
+	
+	@Column(name = "mOrder_amount", nullable = false)
+	private int mOrderAmount;
+	
+	@Column(name = "mOrder_deadline", nullable = false)
+	private Timestamp mOrderDeadline;
+	
+	@Column(name = "mOrder_date", nullable = false)
+	private Timestamp mOrderDate;
+	
+	@Column(name = "mOrder_state", length = 20, nullable = false)
+	private String mOrderState;
+	
+	@Column(name = "mOrder_item", length = 20, nullable = false)
+	private String mOrderItem;
 }
