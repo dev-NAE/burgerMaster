27785b8e1ffe7e9e4ba62846f89f5748f9e143fa
ITWILL <lino3060@naver.com>
2024-10-29 14:56:02 +0900

    qualityList 가상주소 수정

--- src/main/java/com/itwillbs/controller/QualityController.java
+++ src/main/java/com/itwillbs/controller/QualityController.java
@@ -11,7 +11,7 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 public class QualityController {
 
-	@GetMapping("/qualityList")
+	@GetMapping("/quality/qualityList")
 	public String qualityList() {
 		log.info("QualityController qualityList().....");
 		return "/quality/qualityList";
