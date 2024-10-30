f80115bb3578607a97d2b98b47c6448db4d6ecc9
dev-NAE <gkskfh1324@gmail.com>
2024-10-30 09:48:43 +0900

    [24/10/30 강내호] - 재고,입고,출고 파일 생성 및 컨트롤러 경로 지정

--- src/main/java/com/itwillbs/controller/InventoryController.java
+++ src/main/java/com/itwillbs/controller/InventoryController.java
@@ -1,16 +1,69 @@
 package com.itwillbs.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.java.Log;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
+@RequestMapping("/inven")
+@Log
 public class InventoryController {
 
+	// 공통된 View 경로를 상수로 정의
+    private static final String VIEW_PATH = "inventory_management/";
+	
+	
+	//재고 조회
 	@GetMapping("/inventoryList")
-	public String inventoryList() {
+	public String inventoryList(Model model) {
+		log.info("InventroyController inventoryList()");
 		
 		
-		return "inventory/inventoryList";
+		//DB에 저장된 재고들의 목록을 불러옴
+		
+		
+		//model에 저장
+		
+		
+		
+		return VIEW_PATH + "inventory/list";
+	}
+	
+	//입고 등록
+	@GetMapping("/incomingInsert")
+	public String incomingInsert() {
+		
+		
+		return VIEW_PATH + "incoming/insert";
+	}
+	
+	//입고 조회
+	@GetMapping("/incomingList")
+	public String incomingList() {
+		
+		
+		return VIEW_PATH + "incoming/list";
+	}
+	
+	//출고 등록
+	@GetMapping("/outgoingInsert")
+	public String outgoingInsert() {
+		
+		
+		return VIEW_PATH + "outgoing/insert";
+	}
+	
+	//출고 조회
+	@GetMapping("/outgoingList")
+	public String outgoingList() {
+		
+		
+		return VIEW_PATH + "outgoing/list";
 	}
 	
 }
