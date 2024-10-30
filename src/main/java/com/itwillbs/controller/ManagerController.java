36910e1a8cd114d31405ed5d4b28d7644f07596d
Fire-Lights-1s <bs083681@gmail.com>
2024-10-30 17:54:08 +0900

    spring security 권한 설정 완료

--- src/main/java/com/itwillbs/controller/ManagerController.java
+++ src/main/java/com/itwillbs/controller/ManagerController.java
@@ -1,23 +1,46 @@
 package com.itwillbs.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.itwillbs.entity.Manager;
+import com.itwillbs.service.ManagerService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequiredArgsConstructor
 @Log
 public class ManagerController {
+    private final ManagerService managerService;
+
+    @PostMapping(value="/manager/create", produces = "application/text; charset=UTF-8")
+    @ResponseBody
+    public String create(Manager manager) {
+        log.info("ManagerController create()");
+
+        String json = managerService.createManger(manager);;
+        log.info(manager.toString());
+
+        return json;
+    }
     @GetMapping("/login")
     public String login(){
         log.info("AdminsController login()");
         return "managers/login";
     }
 
-    @GetMapping("/managers")
+    @GetMapping("/manager/list")
     public String admins(){
         log.info("AdminsController managers()");
-        log.info("security context "+SecurityContextHolder.getContext().getAuthentication().isAuthenticated());
+        log.info("security context "+SecurityContextHolder.getContext()
+                .getAuthentication().isAuthenticated());
+        log.info("authorities "+SecurityContextHolder.getContext()
+                .getAuthentication().getAuthorities());
         return "managers/managers";
     }
     @GetMapping("error/403")
--- src/main/resources/static/js/admins/admins.js
+++ src/main/resources/static/js/admins/admins.js
@@ -1,8 +1,59 @@
+let token = $("meta[name='_csrf']").attr("content");
+let header = $("meta[name='_csrf_header']").attr("content");
+
 $(document).ready(function(){
-    $("tr").click(function(){
-        $("#reportModal").modal();
+    $(".content tr").click(function(){
+        $("#managerChangeModal").modal();
 
 
     });
+    $("#createManager").click(function(){
+        $("#createModal").modal();
 
+
+    });
 });
+
+//관리자 생성하기
+function createManager(){
+    console.log($("#manager_id_create_modal").val())
+    console.log($("#manager_pass_create_modal").val())
+    console.log($("#manager_name_create_modal").val())
+    console.log($("#manager_phone_create_modal").val())
+    console.log($("#manager_email_create_modal").val())
+    console.log(createCheckString('managerRole'))
+    $.ajax({
+        type: "POST",
+        url : "/manager/create",
+        data : {
+            managerId : $("#manager_id_create_modal").val(),
+            pass : $("#manager_pass_create_modal").val(),
+            name : $("#manager_name_create_modal").val(),
+            phone : $("#manager_phone_create_modal").val(),
+            email : $("#manager_email_create_modal").val(),
+            managerRole : createCheckString('managerRole'),
+        },
+        beforeSend : function(xhr)
+        {   /*데이터를 전송하기 전에 헤더에 csrf값을 설정한다*/
+            xhr.setRequestHeader(header, token);
+        },
+        contentType : "application/x-www-form-urlencoded; charset=utf-8",
+        dataType : "json",
+        success : function(data){
+            let report = data;
+        },error : function(){
+            console.log('관리자 생성 실패');
+        }
+    });
+};
+function createCheckString(name){
+    var len = $('input[name=' + name + ']:checked').length;
+    var checkArr = [];
+    if(len > 1){
+        $("input[name=" + name + "]:checked").each(function(e){
+            var value = $(this).val();
+            checkArr.push(value);
+        })
+    };
+    return checkArr.join(',');
+}
