21986ea193ddb3b32e223a0acd29fccf7f52f582
dutper <dutper1997@gmail.com>
2024-11-04 17:40:58 +0900

    temp5

--- src/main/java/com/itwillbs/controller/MFController.java
+++ src/main/java/com/itwillbs/controller/MFController.java
@@ -1,9 +1,13 @@
 package com.itwillbs.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.itwillbs.entity.MFOrder;
 import com.itwillbs.service.MFService;
 
 import lombok.RequiredArgsConstructor;
@@ -18,7 +22,12 @@ public class MFController {
 	private final MFService mfService;
 	
 	@GetMapping("/orders")
-	public String orders() {
+	public String orders(Model model) {
+		log.info("MFController order()");
+		
+		List<MFOrder> orderList = mfService.getOrderList();
+		
+		model.addAttribute("orderList", orderList);
 		
 		return "/manufacture/orders";
 	}
--- src/main/java/com/itwillbs/service/MFService.java
+++ src/main/java/com/itwillbs/service/MFService.java
@@ -1,7 +1,10 @@
 package com.itwillbs.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.itwillbs.entity.MFOrder;
 import com.itwillbs.repository.MFRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -13,5 +16,11 @@ import lombok.extern.java.Log;
 public class MFService {
 	
 	private final MFRepository mfRepository;
+
+	public List<MFOrder> getOrderList() {
+		log.info("MFService getOrderList()");
+		
+		return mfRepository.findAll();
+	}
 	
 }
