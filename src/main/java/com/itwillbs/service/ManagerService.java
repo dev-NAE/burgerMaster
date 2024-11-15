452a0f367c965ea252b6b3e5c2f684c889aebe08
Fire-Lights-1s <bs083681@gmail.com>
2024-11-15 11:19:56 +0900

    필요 없는 코드 삭제

--- src/main/java/com/itwillbs/service/ManagerService.java
+++ src/main/java/com/itwillbs/service/ManagerService.java
@@ -9,11 +9,8 @@ import lombok.extern.java.Log;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
