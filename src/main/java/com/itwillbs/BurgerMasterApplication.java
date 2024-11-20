a1d2af3be87da986cb172b20df946550025daa2f
dev-NAE <gkskfh1324@gmail.com>
2024-11-20 11:02:22 +0900

    [24/11/20 강내호] aws 한국시간 설정2

--- src/main/java/com/itwillbs/BurgerMasterApplication.java
+++ src/main/java/com/itwillbs/BurgerMasterApplication.java
@@ -1,11 +1,21 @@
 package com.itwillbs;
 
+import java.util.Date;
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class BurgerMasterApplication {
 
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+		System.out.println("현재시각 : " + new Date());
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(BurgerMasterApplication.class, args);
 	}
